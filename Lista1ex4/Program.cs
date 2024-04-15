fe9e0ff3381c2fc57876ba490a6484931dc6f0a5
TaldoGBR <165737345+TaldoGBR@users.noreply.github.com>
2024-04-15 19:31:55 -0300

    Add files via upload

---
+++ Lista1ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int area;
+
+            Console.Write("Digite a altura: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a base: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("A área é: ");
+            area = (a * b) / 2;
+            Console.WriteLine(area);
+        }
+    }
+}
